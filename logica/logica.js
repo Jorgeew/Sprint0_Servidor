0d56afe679bc81b176b36b8a2117a94551da571e
Jorgeew <jorgelarrosaquesada@gmail.com>
2022-10-09 13:23:24 +0200

    Añadiendo Logica con Test

---
+++ logica/logica.js
@@ -0,0 +1,130 @@
+// .....................................................................
+// Logica.js
+// .....................................................................
+const sqlite3 = require("sqlite3")
+// .....................................................................
+// .....................................................................
+module.exports = class Logica {
+	// .................................................................
+	// nombreBD: Texto
+	// -->
+	// constructor () -->
+	// .................................................................
+	constructor(nombreBD, cb) {
+		this.laConexion = new sqlite3.Database(
+			nombreBD,
+			(err) => {
+				if (!err) {
+					this.laConexion.run("PRAGMA foreign_keys = ON")
+				}
+				cb(err)
+			})
+	} // ()
+
+	
+	// .................................................................
+	// nombreTabla:Texto, id: R
+	// -->
+	// borrarFilasConID() -->
+	// .................................................................
+	borrarFilasConID(tabla, id) {
+		return new Promise((resolver, rechazar) => {
+			this.laConexion.run(
+				"delete from " + tabla + " where id = " + id + ";",
+				(err) => (err ? rechazar(err) : resolver())
+			)
+		})
+	} // ()
+	// .................................................................
+	// borrarFilasDeTodasLasTablas() -->
+	// .................................................................
+	async borrarFilasDeTodasLasTablas() {
+		await this.borrarFilasDe("medicion")
+	} // ()
+	
+
+	// .................................................................
+	// datos:{id: null, valor: R, fehca: Texto, nombreSensor: Texto, longitud: R, latitud: R}
+	// -->
+	// insertarMedicion() -->
+	// .................................................................
+	
+	insertarMedicion(datos) {
+		var textoSQL =
+			"insert into medicion values($id, $valor, $fecha, $nombreSensor, $longitud, $latitud);"
+		var valoresParaSQL = {
+			$id: datos.id,
+			$valor: datos.valor,
+			$fecha: datos.fecha,
+			$nombreSensor: datos.nombreSensor,
+			$longitud: datos.longitud,
+			$latitud: datos.latitud
+		}
+		console.log(datos.valor);
+		console.log(datos.fecha);
+		console.log(datos.nombreSensor);
+		console.log(datos.longitud);
+		console.log(datos.latitud);
+		console.log("");
+		
+		// <//> <//> <//> <>/
+		return new Promise((resolver, rechazar) => {
+			this.laConexion.run(textoSQL, valoresParaSQL, function (err) {
+				(err ? rechazar(err) : resolver())
+			})
+		})
+	} // ()
+
+	// .................................................................
+	// id: R
+	// -->
+	// buscarMedicionConID() <--
+	// <--
+	// {id: R, valor: R, fehca: Texto, nombreSensor: Texto, longitud: R, latitud: R}
+	// .................................................................
+	
+	buscarMedicionConID(id) {
+		var textoSQL = "select * from medicion where id=$id";  //$id es un parámetro 
+		var valoresParaSQL = { $id: id } // objeto 
+		return new Promise((resolver, rechazar) => {
+			this.laConexion.all(textoSQL, valoresParaSQL,
+				(err, res) => {
+					(err ? rechazar(err) : resolver(res))
+				})
+		})
+	}
+	// ()
+
+	// .................................................................
+	// mostrarTodasMediciones() <--
+	// <--
+	// {id: R, valor: R, fehca: Texto, nombreSensor: Texto, longitud: R, latitud: R}
+	// .................................................................
+	mostrarTodasMediciones() {
+		var textoSQL = "SELECT * FROM medicion";  //$id es un parámetro 
+		var valoresParaSQL = { } // objeto 
+		return new Promise((resolver, rechazar) => {
+			this.laConexion.all(textoSQL, valoresParaSQL,
+				(err, res) => {
+					(err ? rechazar(err) : resolver(res))
+					//console.log(res);
+				})
+		})
+	}
+
+	// .................................................................
+	// cerrar() -->
+	// .................................................................
+
+	cerrar() {
+		return new Promise((resolver, rechazar) => {
+			this.laConexion.close((err) => {
+				(err ? rechazar(err) : resolver())
+			})
+		})
+	} // ()
+	
+}
+// class
+// .....................................................................
+// .....................................................................
---
+++ logica/test/mainTest.js
@@ -0,0 +1,105 @@
+
+// ........................................................
+// mainTest1.js
+// ........................................................
+const Logica = require("../Logica.js")
+var assert = require("assert")
+// ........................................................
+// main ()
+// ........................................................
+
+// es donde empieza la prueba del test 
+
+describe("Test 1: Funcionamiento de la logica", function () {
+	// ....................................................
+	// ....................................................
+	var laLogica = null
+	// ....................................................
+	// ....................................................
+	it("conectar a la base de datos", function (hecho) {  // la prueba concreta, el cual tiene su tittulo y un callback con una funcion con cualquier nombre. 
+		laLogica = new Logica(  // crear un objeto --> 
+			"../bd/dbmedicion",
+			function (err) {
+				if (err) {
+					throw new Error("No he podido conectar con datos.db")
+				}
+				hecho()
+			})
+	}) 
+    
+    // it
+	// ....................................................
+	// ....................................................
+	//it("borrar todas las filas", async function () {
+	//	await laLogica.borrarFilasDeTodasLasTablas()
+	//})
+
+	// it
+	// ....................................................
+	// ....................................................
+
+	it("puedo insertar una medicion", async function () {
+		try {
+			await laLogica.insertarMedicion(
+				{
+					id: null,
+					valor: 2,
+					fecha: "30/09/2022",
+					nombreSensor: "test.js",
+					longitud: 39,
+					latitud: 0,
+				}
+            )
+			var res = await laLogica.buscarMedicionConID("14")
+			//assert.equal(res.length, 1, "¿no hay un resulado?")  // -->  (a, b, c) -> if (a!=b) --> return c 
+			assert.equal(res[0].valor, 2, "¿no es 32?")
+			
+
+		} catch (error) {
+			error = err;
+		}
+	})
+
+	// it
+	// ....................................................
+	// ....................................................
+
+	it("puedo borrar el daton que he añadido", async function () {
+		try {
+			await laLogica.borrarFilasConID("medicion","14")
+		} catch (error) {
+			error = err;
+		}
+	})
+
+    // it
+	// ....................................................
+	// ....................................................
+
+	it("puedo mostrar todas las medicion", async function () {
+		try {
+			var res = await laLogica.mostrarTodasMediciones()
+			assert.equal(res[0].id, 8, "¿no es 1?")
+			assert.equal(res[0].valor, 55, "¿no es 12?")  // el assert es un if 
+           
+		} catch (error) {
+			error = err;
+		}
+	})
+
+    // it
+	// ....................................................
+	// ....................................................
+	it("cerrar conexión a la base de datos",
+		async function () {
+			try {
+				await laLogica.cerrar()
+			} catch (err) {
+				// assert.equal( 0, 1, "cerrar conexión a BD fallada: " + err)
+				throw new Error("cerrar conexión a BD fallada: " + err)
+			}
+		}
+	) // it
+
+})
+
